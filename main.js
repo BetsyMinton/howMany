76105a69c8e7bf1c576a45c4abbdb244a1c6a326
emm2305 <50798469+emm2305@users.noreply.github.com>
2020-01-12 16:05:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'socks',
+    image: "/Users/mintonb/Documents/GitHub/HowMany/vmSocks-green-onWhite.jpg",
+    altText: "Socks",
+    link: "https://www.amazon.com/dp/B07Z5DFYBD/ref=s9_acsd_ri_bw_c2_x_0_i?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-11&pf_rd_r=75R9GKHJE0K5QBY0VE37&pf_rd_t=101&pf_rd_p=c1be5ed5-742b-487a-82a3-0d286153a44e&pf_rd_i=283155"
+  }
+  })
